b742a44dcc7dec0622807a4c3cb88416653612cd
Nurako <sharankumarajith@gmail.com>
2019-11-07 14:35:45 +0530

    Added humour

--- src/Components/FooterBar.js
+++ src/Components/FooterBar.js
@@ -6,7 +6,8 @@ import { FaReact } from "react-icons/fa";
 function FooterBar() {
     return (   
         <NavBar expand="lg" bg="dark" variant="dark" sticky="bottom">
-            <p className="mx-auto align-middle text-muted">Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"><FaReact /></a> and without <a href="https://www.youtube.com/watch?v=3rzgrP7VA_Q" target="_blank" rel="noopener noreferrer"><TiHeartOutline /></a></p>
+            <p className="mx-auto align-middle text-muted mt-1">Made with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"><FaReact /></a> and without <a href="https://www.youtube.com/watch?v=3rzgrP7VA_Q" target="_blank" rel="noopener noreferrer"><TiHeartOutline /></a></p>
+            <p className ="mx-auto align-middle mt-2"><img class = "mx-auto" src="https://forthebadge.com/images/badges/60-percent-of-the-time-works-every-time.svg" alt=" "/></p>
         </NavBar>
     );
 }
